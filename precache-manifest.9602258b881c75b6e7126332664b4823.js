0b045de5c1ce9682a6d3afe59526a586612254c3
Alejandro AR <alex-ar@outlook.com>
2020-03-11 11:50:08 -0600

    Updates

---
+++ precache-manifest.9602258b881c75b6e7126332664b4823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0301c549954440ca57a06168dbece1",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "3eecd1d78207a2ae75fb",
+    "url": "/Pokedex/static/css/main.d4688878.chunk.css"
+  },
+  {
+    "revision": "ae2694f1d8b8986b8dc9",
+    "url": "/Pokedex/static/js/2.ed0fee62.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Pokedex/static/js/2.ed0fee62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eecd1d78207a2ae75fb",
+    "url": "/Pokedex/static/js/main.c5afc484.chunk.js"
+  },
+  {
+    "revision": "ace2314947c0ba56b2bf",
+    "url": "/Pokedex/static/js/runtime-main.ff5c3d80.js"
+  }
+]);
